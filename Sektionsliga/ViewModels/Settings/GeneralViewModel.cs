13caea41b4477c6034e2aa842e7305e8e129f631
dl-84 <72781732+dl-84@users.noreply.github.com>
2026-05-17 15:31:43 +0200

    Implement general settings page with display language selection

--- Sektionsliga/ViewModels/Settings/GeneralViewModel.cs
+++ Sektionsliga/ViewModels/Settings/GeneralViewModel.cs
@@ -1,3 +1,26 @@
+using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
+
 namespace Sektionsliga.ViewModels.Settings;
 
-public partial class GeneralViewModel : ViewModelBase { }
+public enum DisplayLanguage
+{
+    German,
+    English,
+}
+
+public record LanguageOption(DisplayLanguage Value, string DisplayName);
+
+public partial class GeneralViewModel : ViewModelBase
+{
+    public List<LanguageOption> LanguageOptions { get; } =
+    [new(DisplayLanguage.German, "Deutsch"), new(DisplayLanguage.English, "English")];
+
+    [ObservableProperty]
+    public partial LanguageOption SelectedLanguageOption { get; set; }
+
+    public GeneralViewModel()
+    {
+        SelectedLanguageOption = LanguageOptions[0];
+    }
+}
