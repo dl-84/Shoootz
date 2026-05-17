804d5cea69bc2a80af956dcc4789ad06c1182dc5
dl-84 <72781732+dl-84@users.noreply.github.com>
2026-05-17 12:18:27 +0200

    Add braces to all if blocks per code style

--- Sektionsliga/ViewLocator.cs
+++ Sektionsliga/ViewLocator.cs
@@ -10,7 +10,9 @@ public class ViewLocator : IDataTemplate
     public Control? Build(object? param)
     {
         if (param is null)
+        {
             return null;
+        }
 
         string name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
         Type? type = Type.GetType(name);
